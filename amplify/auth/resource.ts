3c538deb6781332ab5a6167138149724ed03e046
CarlosArrieta <carrieta@defontana.com>
2025-02-02 17:06:16 -0300

    add google provider

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -6,6 +6,16 @@ import { defineAuth } from "@aws-amplify/backend";
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
-  },
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://main.dnxryaxv0wu3i.amplifyapp.com/profile'
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://main.dnxryaxv0wu3i.amplifyapp.com']
+    }
+  }
 });
