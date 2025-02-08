4e230dbddcf39d67c06e31ac34116de29e448097
CarlosArrieta <carrieta@defontana.com>
2025-02-08 16:36:28 -0300

    use client

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,19 @@
+"use client"
 import { client } from "@/lib/client"
 
 export default function TodoList() {
   const createTodo = async () => {
-    await client.models.Todo.create({
-      content: window.prompt("Todo content?"),
-      isDone: false
-    })
+    console.log("Creating todo...")	
+    try{
+      var res = await client.models.Todo.create({
+        content: window.prompt("Todo content?"),
+        isDone: false
+      })
+      console.log(res)
+    }catch(e){
+      console.log(e)
+    }
+    
   }
 
   return <div>
